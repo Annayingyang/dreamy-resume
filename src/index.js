b0764e5614315b21dcb4167bae3ad98a1240dfc8
Annayingyang <2540640@students.wits.ac.za>
2025-09-29 17:30:59 +0200

    configure gh pages deployment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 // src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AppProvider } from "./context/AppContext";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
